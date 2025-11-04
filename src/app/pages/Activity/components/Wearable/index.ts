5647c42c01cd62c4c5a04f3420d0bef4c27411fc
Aurelien972 <contact@imprimeriecolibri.com>
2025-11-04 18:45:36 -0400

    commit

--- src/app/pages/Activity/components/Wearable/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as HeartRateMetricsCard } from './HeartRateMetricsCard';
-export { default as RecoveryScoreCard } from './RecoveryScoreCard';
-export { default as PerformanceMetricsCard } from './PerformanceMetricsCard';
-export { default as WearableBadge } from './WearableBadge';
